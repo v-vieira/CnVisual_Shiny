2d2f6aabf4aa40782d46b523cc67966ca84828b9
v-vieira <54221945+v-vieira@users.noreply.github.com>
2021-06-16 21:14:43 -0300

    Correção erro no clock

--- server.R
+++ server.R
@@ -40,10 +40,21 @@ server <- function(session,input,output){
       }
     }
   })
+  
   # Ao clicar no botao, roda o metodo e ativa o timer
   observeEvent(input$button,{
-    clock <<- reactiveTimer(as.numeric(input$input_veloc_anim)*1000)
-    timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    if(input$input_veloc_anim == ''){
+      clock <<- reactiveTimer(300)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
+    else if (as.numeric(input$input_veloc_anim) < 0.3){
+      clock <<- reactiveTimer(300)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
+    else{
+      clock <<- reactiveTimer(as.numeric(input$input_veloc_anim)*1000)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
     
     plot_vector <- NULL
     value_output <- NULL
