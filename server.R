66aa4fa0ad3f17662ca151d1a3280542437b2376
v-vieira <54221945+v-vieira@users.noreply.github.com>
2021-06-16 21:12:50 -0300

    Evitar erro no clock
    Garantir que o clock sempre esteja preenchido ao clicar em rodar e caso seja menor que 300 milissegundos, travar em 0.3 s.

--- server.R
+++ server.R
@@ -40,10 +40,22 @@ server <- function(session,input,output){
       }
     }
   })
+  
   # Ao clicar no botao, roda o metodo e ativa o timer
   observeEvent(input$button,{
-    clock <<- reactiveTimer(as.numeric(input$input_veloc_anim)*1000)
-    timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    if(input$input_veloc_anim == ''){
+      clock <<- reactiveTimer(300)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
+    else if (as.numeric(input$input_veloc_anim) < 0.3){
+      clock <<- reactiveTimer(300)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
+    else{
+      clock <<- reactiveTimer(as.numeric(input$input_veloc_anim)*1000)
+      timer <<- reactiveValues(inc=0, timer=clock,started=FALSE)
+    }
+    print("aqui")
     
     plot_vector <- NULL
     value_output <- NULL
